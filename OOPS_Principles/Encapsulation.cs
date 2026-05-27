83edefb448da2bb5c724ee04193bc74952f9bbf7
NITISH KUMAR <NITISH KUMAR@LAPTOP-95P3EDGR>
2026-05-27 12:12:11 +0530

    push a new line

--- OOPS_Principles/Encapsulation.cs
+++ OOPS_Principles/Encapsulation.cs
@@ -1,60 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOPS_Principles
-{
-    internal class Encapsulation
-    {
-        class Account
-        {
-            private double balance;
-            private int pin;
+//namespace OOPS_Principles
+//{
+//    internal class Encapsulation
+//    {
+//        class Account
+//        {
+//            private double balance;
+//            private string pin;
 
-        public void deposit(double amount)
-            {
-                if (amount > 0)
-                {
-                    balance += amount;
-                }
-            }
+//        public void deposit(double amount)
+//            {
+//                if (amount > 0)
+//                {
+//                    balance += amount;
+//                }
+//            }
 
-            public void withdraw( double amount)
-            {
-                Console.WriteLine("Enter your pin:");
-                pin = Convert.ToInt32(Console.ReadLine());
+//            public void withdraw( double amount)
+//            {
+//                Console.WriteLine("Enter your pin:");
+//                pin = Console.ReadLine();
                 
 
-                    if (pin == 963963 && amount <= balance)
-                    {
-                        balance -= amount;
-                    }
+//                    if (pin == "963963" && amount <= balance)
+//                    {
+//                        balance -= amount;
+//                    }
                 
-                   else
-                   {
+//                   else
+//                   {
 
-                    Console.WriteLine("please enter  valid pin");
-                   }
+//                    Console.WriteLine("please enter  valid pin");
+//                   }
 
                 
-            }
-            public double getbalance()
-            {
-                return balance;
-            }
-        }
-        static void Main(string[] args)
-        {
-            Account acc=new Account();
-            acc.deposit(6000);
-            acc.withdraw(2000);
-            Console.WriteLine("your  Avaliable balance is:");
-            Console.WriteLine("-----------------------------");
-            Console.WriteLine(acc.getbalance());
-            Console.WriteLine("-----------------------------");
+//            }
+//            public double getbalance()
+//            {
+//                return balance;
+//            }
+//        }
+//        static void Main(string[] args)
+//        {
+//            Account acc=new Account();
+//            acc.deposit(6000);
+//            acc.withdraw(2000);
+//            Console.WriteLine("your  Avaliable balance is:");
+//            Console.WriteLine("-----------------------------");
+//            Console.WriteLine(acc.getbalance());
+//            Console.WriteLine("-----------------------------");
 
-        }
-    }
-}
+//        }
+//    }
+//}
