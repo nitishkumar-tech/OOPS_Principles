e3060561419d822d72a76379c78f3fc0e87abb5c
NITISH KUMAR <NITISH KUMAR@LAPTOP-95P3EDGR>
2026-05-27 10:47:16 +0530

    Add project files.

---
+++ OOPS_Principles/Encapsulation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Principles
+{
+    internal class Encapsulation
+    {
+        class Account
+        {
+            private double balance;
+
+        public void deposit(double amount)
+            {
+                if (amount > 0)
+                {
+                    balance += amount;
+                }
+            }
+
+            public void withdraw( double amount)
+            {
+                if (amount <= balance)
+                {
+                    balance -= amount;
+                }
+            }
+            public double getbalance()
+            {
+                return balance;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Account acc=new Account();
+            acc.deposit(6000);
+            acc.withdraw(2000);
+           Console.WriteLine(acc.getbalance());
+        }
+    }
+}
