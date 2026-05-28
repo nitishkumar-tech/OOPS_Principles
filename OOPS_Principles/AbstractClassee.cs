7034ef1a9a572fa6f8d27c1b7a4bbe3602b35615
NITISH KUMAR <NITISH KUMAR@LAPTOP-95P3EDGR>
2026-05-28 10:54:27 +0530

    push a new line

---
+++ OOPS_Principles/AbstractClassee.cs
@@ -0,0 +1,44 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace OOPS_Principles
+//{
+//    internal class AbstractClassee
+//    {
+//        abstract class abstractclass
+//        {
+//            public void start()
+//            {
+//                Console.WriteLine("started the class");
+//            }
+//            public void resume()
+//            {
+//                Console.WriteLine("resumed the class");
+//            }
+//            public void display()
+//            {
+//                Console.WriteLine("welcome");
+//            }
+//            public abstract void end();
+//        }
+//        class derviedclass : abstractclass
+//        {
+//            public override void end()
+//            {
+//                Console.WriteLine("session has ended");
+//            }
+//        }
+//        static void Main(string[] args)
+//        {
+//            abstractclass obj = new derviedclass();
+//            obj.start();
+//            obj.resume();
+//            obj.display();
+//            obj.end();
+//        }
+//    }
+//}
+
--- OOPS_Principles/Hosiptal_recrd.cs
+++ OOPS_Principles/Hosiptal_recrd.cs
@@ -1,35 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOPS_Principles
-{
-    internal class Hosiptal_recrd
-    {
-        class hosiptals
-        {
-            private string pname;
-            private int page;
-            private string medicalhistory;
-            public string PNAME { get; set; }
-            public int PAGE { get; set; }
-            public void Medicalhistory()
-            {
-                medicalhistory = "Patient has a history of diabetes and hypertension.";
-                Console.WriteLine(medicalhistory);
-            }
-        }
-        static void Main(string[] args)
-        {
+//namespace OOPS_Principles
+//{
+//    internal class Hosiptal_recrd
+//    {
+//        class hosiptals
+//        {
+//            private string pname;
+//            private int page;
+//            private string medicalhistory;
+//            public string PNAME { get; set; }
+//            public int PAGE { get; set; }
+//            public void Medicalhistory()
+//            {
+//                medicalhistory = "Patient has a history of diabetes and hypertension.";
+//                Console.WriteLine(medicalhistory);
+//            }
+
+//        }
+//        static void Main(string[] args)
+//        {
             
-            hosiptals obj = new hosiptals();
-            obj.PNAME = "Rahul";
-            obj.PAGE = 35;
-            Console.WriteLine(obj.PNAME);
-            Console.WriteLine(obj.PAGE);
-            obj.Medicalhistory();
-        }
-    }
-}
+//            hosiptals obj = new hosiptals();
+//            obj.PNAME = "Rahul";
+//            obj.PAGE = 35;
+//            Console.WriteLine(obj.PNAME);
+//            Console.WriteLine(obj.PAGE);
+//            obj.Medicalhistory();
+//        }
+//    }
+//}
