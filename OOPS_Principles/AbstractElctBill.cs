2fca19233e5451f00d604d427d9f39c9ba4610d3
NITISH KUMAR <NITISH KUMAR@LAPTOP-95P3EDGR>
2026-05-29 11:08:54 +0530

    new line pushed

---
+++ OOPS_Principles/AbstractElctBill.cs
@@ -0,0 +1,71 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace OOPS_Principles
+//{
+//    internal class AbstractElctBill
+//    {
+//        abstract class HMWBS
+//        {
+//            public void billdeatils()
+//            {
+//                Console.WriteLine("HMWBS Bill Generated");
+//            }
+//            public abstract void Getbill(double units);
+//        }
+    
+//        class ResidentailCustomers : HMWBS
+//        {
+//            public override void Getbill(double units)
+//            {
+//                double bill = units * 7;
+//                Console.WriteLine(" ResidentailCustomers bill " + bill);
+//            }
+//        }
+//        class CommericalCustomers : HMWBS
+//        {
+//            public override void Getbill(double units)
+//            {
+//                double bill = units * 10;
+//                Console.WriteLine("CommericalCustomers  bill " + bill);
+//            }
+//        }
+//        class AgriculturalCustomers : HMWBS
+//        {
+//            public override void Getbill(double units)
+//            {
+//                double bill = units * 0;
+//                Console.WriteLine("AgriculturalCustomers  bill " + bill);
+//            }
+//        }
+//        class IndustryCustomers : HMWBS
+//        {
+//            public override void Getbill(double units)
+//            {
+//                double bill = units * 15;
+//                Console.WriteLine("IndustryCustomers  bill " + bill);
+//            }
+//        }
+
+//        internal class program
+//        {
+//            static void Main(string[] args)
+//            {
+//                HMWBS obj = new ResidentailCustomers();
+//                obj.billdeatils();
+//                Console.WriteLine("Enter number of units consumed");
+//                double unit=int.Parse(Console.ReadLine());
+//                obj.Getbill(unit);
+//                obj = new CommericalCustomers();
+//                obj.Getbill(unit);
+//                obj=new AgriculturalCustomers();
+//                obj.Getbill(unit);
+//                obj= new IndustryCustomers();
+//                obj.Getbill(unit);
+//            }
+//        }
+//    }
+//}
--- OOPS_Principles/AbstractVirtualCase.cs
+++ OOPS_Principles/AbstractVirtualCase.cs
@@ -1,43 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOPS_Principles
-{
-    internal class AbstractVirtualCase
-    {
-        class myclass
-        {
-            public void display()
-            {
-                Console.WriteLine("Welocme to OOPS");
-            }
-            public virtual void show()
-            {
-                Console.WriteLine("show is calling");
-            }
-        }
-        class dervied:myclass
-        {
-            public void message()
-            {
-                Console.WriteLine("message is calling");
-            }
-            public override void show()
-            {
-                Console.WriteLine("this is override method");
-            }
-        }
-        static void Main(string[] args)
-        {
-            myclass obj = new myclass();
-            obj.display();
-            obj.show();
-            dervied objd= new dervied();
-            objd.message();
-            objd.show();
-        }
-    }
-}
+//namespace OOPS_Principles
+//{
+//    internal class AbstractVirtualCase
+//    {
+//        class myclass
+//        {
+//            public void display()
+//            {
+//                Console.WriteLine("Welocme to OOPS");
+//            }
+//            public virtual void show()
+//            {
+//                Console.WriteLine("show is calling");
+//            }
+//        }
+//        class dervied:myclass
+//        {
+//            public void message()
+//            {
+//                Console.WriteLine("message is calling");
+//            }
+//            public override void show()
+//            {
+//                Console.WriteLine("this is override method");
+//            }
+//        }
+//        static void Main(string[] args)
+//        {
+//            myclass obj = new myclass();
+//            obj.display();
+//            obj.show();
+//            dervied objd= new dervied();
+//            objd.message();
+//            objd.show();
+//        }
+//    }
+//}
