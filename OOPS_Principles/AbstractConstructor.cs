2bd3f4f702181269e709038a381b48e9c6f5efd6
NITISH KUMAR <NITISH KUMAR@LAPTOP-95P3EDGR>
2026-05-29 15:19:33 +0530

    new line pushed

--- OOPS_Principles/AbstractConstructor.cs
+++ OOPS_Principles/AbstractConstructor.cs
@@ -1,50 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOPS_Principles
-{
-    internal class AbstractConstructor
-    {
-        abstract class person
-        {
-            protected string name;
-            private int age;
-            public person(string name, int age)
-            {
-                this.name = name;
-                this.age = age;
-                Console.WriteLine("person constructor is called");
-            }
-            public void Displayinfo()
-            {
-                Console.WriteLine($"Name:{name},Age:{age}");
-            }
-            public abstract void Role();
-        }
-        class student : person
-        {
-            public int rollno;
-            public student(string name, int age, int rollno) : base(name, age)
-            {
-                this.rollno = rollno;
-                Console.WriteLine("student constructor called");
-            }
-            public override void Role()
-            {
-                Console.WriteLine("Iam a student ");
-            }
-        }
-        class program
-        {
-            static void Main(string[] args)
-            {
-                student s = new student("rama", 22, 101);
-                s.Role();
-                s.Displayinfo();
-            }
-        }
-    }
-}
+//namespace OOPS_Principles
+//{
+//    internal class AbstractConstructor
+//    {
+//        abstract class person
+//        {
+//            protected string name;
+//            private int age;
+//            public person(string name, int age)
+//            {
+//                this.name = name;
+//                this.age = age;
+//                Console.WriteLine("person constructor is called");
+//            }
+//            public void Displayinfo()
+//            {
+//                Console.WriteLine($"Name:{name},Age:{age}");
+//            }
+//            public abstract void Role();
+//        }
+//        class student : person
+//        {
+//            public int rollno;
+//            public student(string name, int age, int rollno) : base(name, age)
+//            {
+//                this.rollno = rollno;
+//                Console.WriteLine("student constructor called");
+//            }
+//            public override void Role()
+//            {
+//                Console.WriteLine("Iam a student ");
+//            }
+//        }
+//        class program
+//        {
+//            static void Main(string[] args)
+//            {
+//                student s = new student("rama", 22, 101);
+//                s.Role();
+//                s.Displayinfo();
+//            }
+//        }
+//    }
+//}
---
+++ OOPS_Principles/InhertingAbstarctClass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Principles
+{
+    internal class InhertingAbstarctClass
+    {
+        abstract class vehicle
+        {
+            protected string vehiclename;
+            public vehicle(string vehiclename)
+            {
+                this.vehiclename = vehiclename;
+
+                Console.WriteLine("vehicle is called");
+            }
+            public void show_vehicle_details()
+            {
+                Console.WriteLine($"vehicle name is {vehiclename}");
+            }
+            public abstract void vehciletype();
+            }
+        abstract class vehiclemodel : vehicle
+        {
+            protected string modelname;
+            public vehiclemodel(string vehiclename, string modelname) : base(vehiclename)
+            {
+                this.modelname = modelname;
+                Console.WriteLine("Vehicle model IS CAlling");
+            }
+            public void displayName()
+            {
+                Console.WriteLine($"Model name {modelname}");
+            }
+            public abstract void modeltype();
+            }
+        class vehicledata : vehiclemodel
+        {
+            public int vehiclenum;
+            public vehicledata(string vehiclename, string vehiclemodel, int vehiclenum) : base(vehiclename, vehiclemodel)
+            {
+                this.vehiclenum = vehiclenum;
+                Console.WriteLine("vehicledata is called");
+            }
+            public override void vehciletype()
+            {
+                Console.WriteLine("cars");
+            }
+            public override void modeltype()
+            {
+                Console.WriteLine("electric");
+            }
+        }
+        internal class program
+        {
+            static void Main(string[] args)
+            {
+                vehicledata vd = new vehicledata("AUDI", "Q5", 9999);
+                vd.show_vehicle_details();
+                vd.displayName();
+                vd.vehciletype();
+                vd.modeltype();
+            }
+        }
+    }
+}
